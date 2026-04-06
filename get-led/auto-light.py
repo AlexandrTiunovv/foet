5ac12d04623cc6686c4e2ca4eb5c58c769d6c795
AlexandrTiunovv <sashunya.tiunov@list.ru>
2026-04-06 19:17:54 +0300

    ...

---
+++ get-led/auto-light.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+led = 26
+GPIO.setup(led, GPIO.OUT)
+signal = 6
+GPIO.setup(signal, GPIO.IN)
+while True:
+    if GPIO.input(signal):
+        GPIO.output(led, 0)
+    else:
+        GPIO.output(led, 1)
